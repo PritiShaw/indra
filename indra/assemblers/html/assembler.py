5ce6e395a5f59270b11a31fcc13c6cc712d92d9a
Priti Shaw <pritishaw0103@gmail.com>
2020-07-15 21:22:42 +0530

    Update assembler.py

--- indra/assemblers/html/assembler.py
+++ indra/assemblers/html/assembler.py
@@ -324,11 +324,11 @@ class HtmlAssembler(object):
             all statements placed under a single heading.
         add_full_text_search_link : bool
             If True, link with Text fragment search in PMC journal will be
-            added for the statements.
-        
-                All other keyword arguments are passed along to the template. If you
-                are using a custom template with args that are not passed below, this
-                is how you pass them.
+            added for the statements.  
+        **template_kwargs
+            All other keyword arguments are passed along to the template. If you
+            are using a custom template with args that are not passed below, this
+            is how you pass them.
 
         Returns
         -------
